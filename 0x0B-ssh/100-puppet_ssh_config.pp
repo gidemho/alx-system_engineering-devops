d47a9bc154b88bced0d1c968f1aa3fc460039406
gidemho <gidemho@mail.com>
2024-04-18 06:46:43 -0700

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,14 @@
 # using puppet to make changes to our configuration file
 
 file { '/etc/ssh/ssh_config';
+
 	ensure => present,
 
-content => "#SSH client configuration\n
-	Host \n*
-	IdentityFile ~/.ssh/school\n
-	PasswordAuthentication no\n",
+	content => "# SSH client configuration\n
 
+	Host *\n
+
+		IdentityFile ~/.ssh/school\n
+
+		PasswordAuthentication no\n",
 }
