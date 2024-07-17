d5874c8adc2c6dd23bee230b3c6db77a7b0b053a
gidemho <gidemho@gmail.com>
2024-07-17 19:23:21 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""Return the total number of subscribers on a given subreddit."""
-	url =
-"https://ww.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {"user-agent": "Mozilla/5.0"}
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	if response.status_code == 200:
-	data = response.json()
-	subscribers = data['data']['subscribers']
-return subscribers
-	else:
-return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
